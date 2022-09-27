d6ef03216672122214cebba207e47e834a113b23
tahsinkarim <tahsinnkarim@gmail.com>
2022-09-28 02:24:42 +0600

    updated cart component

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,11 +2,25 @@ import React from 'react';
 import './Cart.css';
 
 const Cart = (props) => {
-    const {cart} = props
+    const {cart} = props;
+    let total = 0;
+    let shipping = 0;
+    for(const product of cart){
+        total = total + product.price
+        shipping = shipping + product.shipping
+    }
+    const tax = total * 0.1
+
     return (
         <div className="cart-container">
-            <h4>Order summery</h4>
-            <p>Selected items: {cart.length}</p>
+            <div className="cart">
+                <h3 className='cart-title'>Order summery</h3>
+                <p>Selected items: {cart.length}</p>
+                <p>Total price: ${total}</p>
+                <p>Total shipping: ${shipping}</p>
+                <p>Tax: ${tax.toFixed(2)}</p>
+                <p>Grand Total:{}</p>
+            </div>
         </div>
     );
 };
